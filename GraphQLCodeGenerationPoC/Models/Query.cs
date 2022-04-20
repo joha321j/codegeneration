36496eb85d2d1fd08c97a1c92cb0cc953884e809
double-em <8335996+double-em@users.noreply.github.com>
2022-04-20 11:07:31 +0200

    Add PoC of manual generation

---
+++ GraphQLCodeGenerationPoC/Models/Query.cs
@@ -0,0 +1,75 @@
+﻿using System.Runtime.Serialization;
+
+namespace GraphQLCodeGenerationPoC.Models;
+
+public class Query
+{
+    // One method per Source should be generated
+    public Azure FetchFromAzure()
+    {
+        return new Azure();
+    }
+}
+
+// One class per source with the containing get methods should be generated
+public class Azure
+{
+    [GraphQLName("metricsResult")]
+    public MetricsResult GetMetric()
+    {
+        return new MetricsResult
+        {
+            Value = new MetricsResultInfo
+            {
+                Data = "Lulullulul"
+            }
+        };
+    }
+}
+
+[DataContract]
+public class MetricsResult
+{
+    [DataMember(Name="value", EmitDefaultValue=false)]
+    public MetricsResultInfo Value { get; set; }
+}
+
+[DataContract]
+public class MetricsResultInfo
+{
+    [DataMember(Name="data", EmitDefaultValue=false)]
+    public string Data { get; set; }
+}
+
+
+// public class Query
+// {
+//     [GraphQLType(typeof(User))]
+//     [GraphQLName("getUser")]
+//     public User GetUser()
+//     {
+//         // return new { name = "Karsten", title = "Cunt" };
+//         return new User("Karsten", "Cunt");
+//     }
+//     
+//     [GraphQLType(typeof(ObjectType))]
+//     [GraphQLName("getDepartment")]
+//     public object GetDepartment()
+//     {
+//         return new { name = "Derp" };
+//     }
+// }
+//
+// public class User : ObjectType
+// {
+//     public User(string Name, string Title)
+//     {
+//         this.Name = Name;
+//         this.Title = Title;
+//     }
+//
+//     public string Name { get; init; }
+//     public string Title { get; init; }
+//     
+//     
+// }
